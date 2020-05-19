81d91220f4472489b28b78486372b9f5c0844e43
saltychess <37352173@qq.com>
2020-05-19 22:19:05 +0800

    Initial Commit

---
+++ ios_HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_HW4
+//
+//  Created by User08 on 2020/5/19.
+//  Copyright © 2020 00657012. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
